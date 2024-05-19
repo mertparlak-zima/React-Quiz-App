ab294cf76fbbca189827e42339bc4461a7eab278
Mert <mertparlak123@hotmail.com>
2024-05-19 12:55:42 +0300

    add ready screen components and refactor main screen and add Loader Components and Error components for data Fetching

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
+import { useEffect, useReducer } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import Ready from "./components/Ready";
 
+function reducer(state, action) {
+  try {
+    switch (action.type) {
+      case "dataRecived":
+        return { ...state, questions: action.payload, status: "ready" };
+      case "failed":
+        return { ...state, status: "error" };
+      case "dataFailed":
+        return { ...state, status: "error" };
+      default: {
+        throw new Error(`Unsupported action type ${action.type}`);
+      }
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
 export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const questionsLength = questions.length;
+
+  useEffect(() => {
+    fetch("http://localhost:8080/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecived", payload: data }))
+      .catch(() => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
       <Header />
-      <Main></Main>
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <Ready questionsLength={questionsLength} />}
+      </Main>
     </div>
   );
 }
