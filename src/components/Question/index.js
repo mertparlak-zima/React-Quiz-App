4f92a34509f43f51fdcb54886e2889592402d8f2
Mert <mertparlak123@hotmail.com>
2024-05-24 16:17:54 +0300

    refactor components structer

--- src/components/Question/index.js
+++ src/components/Question/index.js
@@ -1,7 +1,6 @@
-import Option from "./Option/Option";
+import Option from "../Option";
 
 function Question({ question }) {
-  console.log(question);
   return (
     <div>
       <h4>{question.question}</h4>
